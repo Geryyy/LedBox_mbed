7281a9ba95a3cbd94ebca66b0cd41aeca723b68a
Gerald Ebmer <gerald.ebmer@outlook.com>
2018-02-06 09:42:38 +0100

    DeviceStats module erstellt

---
+++ BSP/DeviceStats.cpp
@@ -0,0 +1,5 @@
+/* @file: DeviceStats.cpp
+ * @author: Gerald Ebmer
+ * @date: 06.02.2018
+ * @brief: device statistics module implementation
+ */
---
+++ BSP/DeviceStats.h
@@ -0,0 +1,12 @@
+/* @file: DeviceStats.h
+ * @author: Gerald Ebmer
+ * @date: 06.02.2018
+ * @brief: device statistics module
+ */
+#ifndef DEVICESTATS_H
+#define DEVICESTATS_H
+
+
+
+
+#endif // DEVICESTATS_H
