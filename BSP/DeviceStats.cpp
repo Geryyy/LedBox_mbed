502e0dd1fc55ba72ff9f6357c8637d3a04e2cb3d
Gerald Ebmer <gerald.ebmer@outlook.com>
2018-02-06 11:33:39 +0100

    flash programmierung in arbeit

--- BSP/DeviceStats.cpp
+++ BSP/DeviceStats.cpp
@@ -3,3 +3,37 @@
  * @date: 06.02.2018
  * @brief: device statistics module implementation
  */
+
+#include "mbed.h"
+#include "DeviceStats.h"
+
+// #if !defined(POST_APPLICATION_ADDR)
+// #error "target.restrict_size must be set for your target in mbed_app.json"
+// #endif
+
+FlashIAP flash;
+
+ResetCounter_t resetCounter;
+
+void procResetCounter(){
+    // load flash content
+
+    // inkrement reset counter
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_OBLRST) != RESET)     resetCounter.OBLRST++;
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST) != RESET)     resetCounter.NRST++;
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST) != RESET)     resetCounter.PORRST++;
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST) != RESET)     resetCounter.SWRST++;
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST) != RESET)    resetCounter.IWDRST++;
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST) != RESET)    resetCounter.WWDRST++;
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST) != RESET)    resetCounter.LPRST++;
+
+    // store reset counter
+
+}
+
+void clearResetCounter(){
+    // set reset counter = 0
+
+    // store reset counter in flash
+
+}
--- BSP/DeviceStats.h
+++ BSP/DeviceStats.h
@@ -6,7 +6,16 @@
 #ifndef DEVICESTATS_H
 #define DEVICESTATS_H
 
+#include "stm32l1xx_hal.h"
 
-
+typedef struct ResetCounter_s{
+    uint32_t OBLRST; // Options bytes loading reset flag
+    uint32_t NRST; // NRST Pin reset
+    uint32_t PORRST; // POR/PDR reset
+    uint32_t SWRST; // Software reset
+    uint32_t IWDRST; // Independent Watchdog reset
+    uint32_t WWDRST; // Window watchdog reset
+    uint32_t LPRST; // low-power reset
+}ResetCounter_t;
 
 #endif // DEVICESTATS_H
